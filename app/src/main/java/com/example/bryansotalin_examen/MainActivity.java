58c31932fa51ce410f285d466c097e3c9c62b3a5
master <z0ta@hotmail.com>
2019-12-05 18:08:19 -0500

    examen

---
+++ app/src/main/java/com/example/bryansotalin_examen/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.bryansotalin_examen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
